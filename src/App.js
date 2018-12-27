cf56c5cc18e4d4d724e6fcbd3fa126d72aeaba92
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2018-12-27 16:22:11 +0530

    commit project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+// import PropTypes from 'prop-types';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello Worlds</h1>
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <h1>My Todos</h1>
+  </div>
+);
+
+App.propTypes = {};
 
 export default App;
